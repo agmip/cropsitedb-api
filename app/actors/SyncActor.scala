cea33688de0106728a3ea53215095590a2abd740
Christopher Villalobos <christopher.villalobos@gmail.com>
2015-04-08 06:45:51 -0400

    POC ping
    Proof of concept of PING function. Added Akka-Agents to support
    per client success/failure rating.

--- app/actors/SyncActor.scala
+++ app/actors/SyncActor.scala
@@ -1,17 +1,31 @@
 package cropsitedb.actors
 
 import akka.actor.{Actor,Props}
+import akka.agent.Agent
 import akka.event.Logging
 import play.api.libs.ws._
 
 import scala.concurrent.Future
 import play.api.Play.current
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object SyncMessages {
 }
 
+// This will work for static endpoints, but dynamic
+// ones will need some conceptual work. Probably
+// along the lines of an add/remove to the SyncRouter
+class SyncRouter(endpoints: List[String]) extends Actor {
+  val log = Logging(context.system, thisl)
+  def receive = {
+    case _ => log.info("we're good!")
+  }
+}
+
 class SyncActor extends Actor {
+  case class Status(current: Option[String], count: Int)
   val log = Logging(context.system, this)
+  val agent = Agent(Status(None, 0))
 
   def receive = {
     case "tick" => pingMyself
@@ -19,9 +33,11 @@ class SyncActor extends Actor {
   }
 
   def pingMyself = {
-    import scala.concurrent.ExecutionContext.Implicits.global
     WS.url("http://localhost:9000/cropsitedb/2/sync/ping").get().map { res =>
       log.info(res.body)
+      agent send (x => Status(Some("alive"), x.count + 1))
+      val cs = agent.get
+      log.info(cs.count+" - "+cs.current.getOrElse("unknown"))
     }
   }
 }
--- build.sbt
+++ build.sbt
@@ -16,5 +16,6 @@ libraryDependencies ++= Seq(
   "org.postgresql" % "postgresql" % "9.3-1102-jdbc41",
   "org.agmip.ace"  % "ace-core"   % "2.0-SNAPSHOT",
   "org.apache.tika"% "tika-core"  % "1.6",
-  "org.agmip"      % "dome"       % "1.4.7"
+  "org.agmip"      % "dome"       % "1.4.7",
+  "com.typesafe.akka" %% "akka-agent" % "2.3.2"
 )
